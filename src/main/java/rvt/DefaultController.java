693b4113e3d52e2795f0d17eb74ed36ecd904e81
22DP1RMiga <131353118+22DP1RMiga@users.noreply.github.com>
2023-12-12 13:59:04 +0000

    Trešās izmaiņas 12.12.2023.

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -6,13 +6,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
     
    @GetMapping(value = "/about")
-    String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
-        return "index";
+   ModelAndView about() {
+    CsvManager manager = new CsvManager("data/hobbies.csv");
+
+    List hobbies = manager.getAllHobbies();
+
+    ModelAndView modelAndView = new ModelAndView("about");
+    modelAndView.addObject("hobbies", )
+
+    String name = "Ralfs";
+    String surname = "Migals";
+    int age = 18;
+    String group = "DP2-1";
+
+    modelAndView.addObject("name", name);
+    modelAndView.addObject("surname", surname);
+    modelAndView.addObject("age", age);
+    modelAndView.addObject("group", group);
+
+    return modelAndView;
+   }
+    /* String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
+        return "about";
     }
     
     String aboutMeAction(Model info) {
@@ -28,4 +49,5 @@ public class DefaultController {
 
         return "about";
     }
+    */
 }
