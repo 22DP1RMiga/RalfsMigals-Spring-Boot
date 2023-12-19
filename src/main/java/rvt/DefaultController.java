ed512eb74d24e110e4afb5e1b224c9712ece24cb
22DP1RMiga <131353118+22DP1RMiga@users.noreply.github.com>
2023-12-19 13:56:16 +0000

    Labi veiktas izmaiņas 19.12.2023.

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -7,47 +7,59 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import java.util.HashMap;
 
 @Controller
 public class DefaultController {
     
    @GetMapping(value = "/about")
-   ModelAndView about() {
-    CsvManager manager = new CsvManager("data/hobbies.csv");
+   ModelAndView about(
+        @RequestParam HashMap<String, String> allParams
+   ) {
 
-    List hobbies = manager.getAllHobbies();
+    String Name = allParams.get("name");
+    String Surname = allParams.get("surname");
+    String Age = allParams.get("age");
+    String Group = allParams.get("group");
+
+    /* System.out.println(
+        allParams.get("name")
+    ); */
 
     ModelAndView modelAndView = new ModelAndView("about");
-    modelAndView.addObject("hobbies", )
-
-    String name = "Ralfs";
-    String surname = "Migals";
-    int age = 18;
-    String group = "DP2-1";
-
-    modelAndView.addObject("name", name);
-    modelAndView.addObject("surname", surname);
-    modelAndView.addObject("age", age);
-    modelAndView.addObject("group", group);
+    modelAndView.addObject("name", Name);
+    modelAndView.addObject("surname", Surname);
+    modelAndView.addObject("age", Age);
+    modelAndView.addObject("group", Group);
 
     return modelAndView;
    }
-    /* String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
-        return "about";
-    }
+
+   // HashMap works just like a dictionary in Python
+//    HashMap<String, String> car1 = new HashMap<>();
+//    car1.put("brand", "Ford");
+//    car1.put("model", "Mustang");
+//    car1.put("year", "1984");
+   
+//    car1.get("brand");  // Output: Ford
+//    modelAndView.addObject("hobbies");
+
+//    String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
+//         return "about";
+//     }
     
-    String aboutMeAction(Model info) {
-        String name = "Ralfs";
-        String surname = "Migals";
-        int age = 18;
-        String group = "DP2-1";
+//     String aboutMeAction(Model info) {
+//         String name = "Ralfs";
+//         String surname = "Migals";
+//         int age = 18;
+//         String group = "DP2-1";
 
-        info.addAttribute("name", name);
-        info.addAttribute("surname", surname);
-        info.addAttribute("age", age);
-        info.addAttribute("group", group);
+//         info.addAttribute("name", name);
+//         info.addAttribute("surname", surname);
+//         info.addAttribute("age", age);
+//         info.addAttribute("group", group);
 
-        return "about";
-    }
-    */
+//         return "about";
+//     }
+    
 }
--- src/main/java/rvt/Hobby.java
+++ src/main/java/rvt/Hobby.java
@@ -12,7 +12,13 @@ public class Hobby {
         this.title = title;
         this.description = description;
     }
-    public int getId() {}
-    public String getTitle() {}
-    public String getDescription() {}
+    public int getId() {
+        return this.id;
+    }
+    public String getTitle() {
+        return this.title;
+    }
+    public String getDescription() {
+        return this.description;
+    }
 }
