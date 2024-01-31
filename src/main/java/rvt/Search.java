c0617e69dd754bb21c4491751ea1bcdf28f3ca48
22DP1RMiga <131353118+22DP1RMiga@users.noreply.github.com>
2024-01-31 13:02:24 +0000

    Beta kods 31.01.2024.

--- src/main/java/rvt/Search.java
+++ src/main/java/rvt/Search.java
@@ -4,5 +4,26 @@ public class Search {
     public static int binarySearch(int[] array, int valueToSearch) {
         int startIndex = 0;
         int endIndex = array.length - 1;
+
+        int middle = (startIndex + endIndex);
     }
 }
+
+// assuming the variable searched exits
+// assuming the variable list exits
+begin = 0 // the 0th index of the list (i.e, the first index of the list)
+end = size(list) - 1 // the last index in the list
+
+repeat until begin is larger than end:
+    middle = (end + begin) / 2
+
+    if the value at list[middle] is searched
+        return the value of the variable middle
+
+    if the value at list[middle] is smaller than searched
+        begin = middle + 1
+
+    if the value at list[middle] is larger than searched
+        end = middle - 1
+
+return value -1
